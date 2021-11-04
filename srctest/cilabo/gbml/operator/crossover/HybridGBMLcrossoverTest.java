fd5f4086542995ecec0911d0b6be1e4aae728d91
omozaki <Uone.1997@outlook.jp>
2021-11-04 19:45:34 +0900

    tested Michigan operation on HybridGBMLcrossover

--- srctest/cilabo/gbml/operator/crossover/HybridGBMLcrossoverTest.java
+++ srctest/cilabo/gbml/operator/crossover/HybridGBMLcrossoverTest.java
@@ -12,7 +12,10 @@ import org.uma.jmetal.util.pseudorandom.JMetalRandom;
 import cilabo.data.DataSet;
 import cilabo.fuzzy.classifier.operator.classification.Classification;
 import cilabo.fuzzy.classifier.operator.classification.factory.SingleWinnerRuleSelection;
+import cilabo.gbml.problem.impl.ProblemMichiganFGBML;
 import cilabo.gbml.problem.impl.pittsburgh.MOP1;
+import cilabo.gbml.solution.PittsburghSolution;
+import cilabo.gbml.solution.util.attribute.ErroredPatternsAttribute;
 import cilabo.main.Consts;
 import cilabo.utility.Input;
 
@@ -21,7 +24,7 @@ public class HybridGBMLcrossoverTest {
 	public void testExecute() {
 		String sep = File.separator;
 		// Load "Pima" dataset
-		String dataName = "dataset" + sep + "pima" + sep + "a0_0_pima-10tra.dat";
+		String dataName = "dataset" + sep + "iris" + sep + "a0_0_iris-10tra.dat";
 		DataSet train = new DataSet();
 		Input.inputSingleLabelDataSet(train, dataName);
 
@@ -39,11 +42,23 @@ public class HybridGBMLcrossoverTest {
 		solutions.add(parent1);
 		solutions.add(parent2);
 
+		// Michigan Evaluation
+		ProblemMichiganFGBML<IntegerSolution> michiganProblem = new ProblemMichiganFGBML<>(Consts.RAND_SEED, train);
+		michiganProblem.michiganEvaluate(((PittsburghSolution)parent1).getMichiganPopulation());
+		michiganProblem.michiganEvaluate(((PittsburghSolution)parent2).getMichiganPopulation());
+
+		// Add errored patterns to parents
+		List<Integer> erroredPatternsId = new ArrayList<>();
+		erroredPatternsId.add(0);
+		erroredPatternsId.add(2);
+		parent1.setAttribute((new ErroredPatternsAttribute<>()).getAttributeId(), erroredPatternsId);
+		parent2.setAttribute((new ErroredPatternsAttribute<>()).getAttributeId(), erroredPatternsId);
+
 		// Crossover
 		double probability = 1.0;
 //		double michiganOperationProbability = Consts.MICHIGAN_OPE_RT;
-		double michiganOperationProbability = 1.0;	// Only Michigan operation
-//		double michiganOperationProbability = 0.0;	// Only Pittsburgh operation
+//		double michiganOperationProbability = 1.0;	// Only Michigan operation: OK
+		double michiganOperationProbability = 0.0;	// Only Pittsburgh operation
 
 		/* Michigan operation*/
 		CrossoverOperator<IntegerSolution> michiganX = new MichiganOperation(Consts.MICHIGAN_CROSS_RT,
@@ -57,5 +72,8 @@ public class HybridGBMLcrossoverTest {
 
 		List<IntegerSolution> offspring = crossover.execute(solutions);
 		System.out.println(offspring);
+
+		michiganProblem.michiganEvaluate(((PittsburghSolution)offspring.get(0)).getMichiganPopulation());
+		System.out.println();
 	}
 }
