71a1766673ac4680d183cfce928ee7033364a67e
omozaki <Uone.1997@outlook.jp>
2021-11-14 22:06:30 +0900

    make MultiTasking_Main.java

--- src/cilabo/labo/developing/multitasking/MultiTasking_Main.java
+++ src/cilabo/labo/developing/multitasking/MultiTasking_Main.java
@@ -5,10 +5,14 @@ import java.io.FileNotFoundException;
 import java.util.Date;
 
 import org.uma.jmetal.util.JMetalException;
+import org.uma.jmetal.util.pseudorandom.JMetalRandom;
 
+import cilabo.data.DataSet;
+import cilabo.data.impl.TrainTestDatasetManager;
 import cilabo.main.Consts;
 import cilabo.utility.Output;
 import cilabo.utility.Parallel;
+import cilabo.utility.Random;
 
 public class MultiTasking_Main {
 
@@ -55,6 +59,35 @@ public class MultiTasking_Main {
 		System.out.println("==== EXPERIMENT =====");
 		Date start = new Date();
 		System.out.println("START: " + start);
+
+		/* Random Number ======================= */
+		Random.getInstance().initRandom(Consts.RAND_SEED);
+		JMetalRandom.getInstance().setSeed(Consts.RAND_SEED);
+
+		/* Load Dataset ======================== */
+		TrainTestDatasetManager datasetManager = new TrainTestDatasetManager();
+		datasetManager.loadTrainTestFiles(CommandLineArgs.trainFile, CommandLineArgs.testFile);
+
+		/* Run MoFGBML algorithm =============== */
+		DataSet train = datasetManager.getTrains().get(0);
+		DataSet test = datasetManager.getTests().get(0);
+		MultiTaskingMoFGBML(train, test);
+		/* ===================================== */
+
+		Date end = new Date();
+		System.out.println("END: " + end);
+		System.out.println("=====================");
+		/* ********************************************************* */
+
+		System.exit(0);
+	}
+
+	/**
+	 *
+	 * @param train
+	 * @param test
+	 */
+	public static void MultiTaskingMoFGBML(DataSet train, DataSet test) {
 
 	}
 }
