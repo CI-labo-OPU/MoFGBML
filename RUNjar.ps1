0a1664fe354bfe4a234b821d08717b51172486a8
PRATI\hirot <hiro.tacky.3362@gmail.com>
2021-12-13 12:50:50 +0900

    no message

---
+++ RUNjar.ps1
@@ -0,0 +1,16 @@
+$dataName = "iris"
+$parallelCores = 12
+$algorithmID = "FAN2021"
+$experimentID = "trial"
+$logFileName = $algorithmID + "_log.txt"
+
+Start-Transcript ./results/$algorithmID/$dataName/$logFileName -Append
+for($i=0; $i -lt 3; $i++){
+	for($j=0; $j -lt 10; $j++){
+		$DtraFileName = "a" + $i + "_" + $j + "_" + $dataName + "-10tra.dat"
+		$DtstFileName = "a" + $i + "_" + $j + "_" + $dataName + "-10tst.dat"
+		Java -jar target/MoFGBML-23.0.0-SNAPSHOT.jar $dataName $algorithmID $experimentID$i$j $parallelCores dataset\$dataName\$DtraFileName dataset\$dataName\$DtstFileName
+		Write-Output "Java -jar target/MoFGBML-23.0.0-SNAPSHOT.jar $dataName $algorithmID $experimentID$i$j $parallelCores dataset\$dataName\$DtraFileName dataset\$dataName\$DtstFileName"
+	}
+}
+Pause
